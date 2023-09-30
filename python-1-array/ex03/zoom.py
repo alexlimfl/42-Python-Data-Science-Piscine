b4610d3259d92d173783201664b601fc9f649e7c
alexlimfl <alexlimfl23@gmail.com>
2023-09-30 23:02:53 +0800

    up

--- python-1-array/ex03/zoom.py
+++ python-1-array/ex03/zoom.py
@@ -4,17 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def main():
     """zoom-in image"""
     pixel_arr = ft_load("animal.jpeg")
-    img_shape = pixel_arr.shape
-    img = Image.fromarray(pixel_arr)
     print(pixel_arr)
-    print(img_shape)
+    img = Image.fromarray(pixel_arr)
 
     width, height = img.size
-    print(width, height)
+    # print(width, height)
 
     crop_size = 400
     x_coor = 140
@@ -23,11 +20,17 @@ def main():
     top = (height // 2 - crop_size//2) + y_coor
     right = left + crop_size
     bottom = top + crop_size
-    print(left, top, right, bottom)
+    # print(left, top, right, bottom)
+
     img2 = img.crop((left, top, right, bottom))
     img3 = img2.convert("L")
-    # gray_s.show()
-    # img2.show()
+    img3_arr = np.array(img3)
+    img3_shape2D = img3_arr.shape
+    img3_arr_exp = np.expand_dims(img3_arr, axis=-1)
+    img3_shape3D = img3_arr_exp.shape
+
+    print(f"New shape after slicing: {img3_shape2D} or {img3_shape3D}")
+    print(img3_arr_exp)
     plt.imshow(img3, cmap="gray")
     plt.show()
 
